958968b7bad17dd29fb009a92dd206c632cb26a4
Bohdan Yurash <yurash.bogdan@gmail.com>
2023-12-25 20:11:33 +0200

    bugs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
-const i = 3 + 7;
-console.log(i);
+import * as http from "http";
+import { get } from "./methods/get.js";
+import { post } from "./methods/post.js";
+import { put } from "./methods/put.js";
+import { deleteR } from "./methods/delete.js";
+import { posts } from "./database.js";
+import { getBody } from "./getBody.js";
+
+const PORT = process.env.PORT || 4000;
+
+const server = http.createServer();
+
+server.on("request", (request, response) => {
+    request.posts = posts;
+    request.query = new URL(request.url, `http://${request.headers.host}`);
+
+    switch (request.method) {
+        case "GET":
+            getBody(request, response, get);
+            break;
+
+        case "POST":
+            getBody(request, response, post);
+            break;
+
+        case "PUT":
+            getBody(request, response, put);
+            break;
+
+        case "DELETE":
+            getBody(request, response, deleteR);
+            break;
+
+        default:
+            // Send response for requests with no other response
+            response.statusCode = 400;
+            response.write("No Response");
+            response.end();
+    }
+});
+
+server.listen(PORT, (err) => {
+    err ? console.error(err) : console.log(`listening on port ${PORT}`);
+});
