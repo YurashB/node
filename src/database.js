b5b40c21b56973de713c837330453350c4ea0975
Bohdan Yurash <yurash.bogdan@gmail.com>
2023-12-25 17:32:05 +0200

    lab 2

---
+++ src/database.js
@@ -0,0 +1,24 @@
+const posts = [
+  {
+    id: 1,
+    title: "The first post",
+    body: "Body of the 1 post",
+  },
+  {
+    id: 2,
+    title: "The second post",
+    body: "Body of the 2 post",
+  },
+  {
+    id: 3,
+    title: "The third post",
+    body: "Body of the 3 post",
+  },
+  {
+    id: 4,
+    title: "The fours post",
+    body: "Body of the 4 post",
+  },
+];
+
+export { posts };
